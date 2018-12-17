73a3c8553c16b2abc4e9e2a24220e833df160cf4
Johndeere96 <henrotpaul@johndeere.com>
2018-12-17 17:31:13 +0100

    BE24 Line Tracing AKA Ray-Casting

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -41,13 +41,8 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	PlayerViewPointRotation = GetOwner()->GetInstigatorController()->GetControlRotation();
 
 	
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s  Rotation: %s"), 
-		*PlayerViewPointLocation.ToString(), 
-		*PlayerViewPointRotation.ToString()
-	);
 	
-	
-
+	// Draw a red trace in the world to visualize
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 	DrawDebugLine(
 		GetWorld(),	
@@ -59,12 +54,28 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0.f, 
 		10.f
 	);
-	// Draw a red trace in the world to visualize
+	
+	//Set up querry parrameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
 
-
-	// Ray-cast out to reach distance
-
-
+	/// Ray-cast out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+	
 	// See what we hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()));
+	}
+	
+
+
 }
 
